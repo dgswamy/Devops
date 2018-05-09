670f41d60609334e584eb57b029f4ed32576f17f
dgswamy <dgswamy001@gmial.com>
2018-05-09 17:33:25 +0530

    js practice files

---
+++ javascriptfiles/script.js
@@ -0,0 +1,63 @@
+//var myheader=document.getElementById("header");
+/*var tagname=document.getElementsByTagName("a");
+var listitems=document.getElementsByTagName("li");
+//var classname=document.getElementsByClassName("navigation");
+console.log("this is nav page:",header.nodeType);
+console.log(header.innerHTML);
+console.log(header.childNodes.length);
+
+console.log("links: ",tagname.length);
+console.log("lists: " ,listitems.length);
+//console.log("navigation: ", navigation.document.getElementsByTagName("ul"));*/
+
+
+/*var headecon=document.getElementById("wrapper");
+header.setAttribute("align","middle");
+*/
+/* creating elements with js and appending 
+var newHeading=document.createElement("h1");
+var newpara=document.createElement("p");
+
+newHeading.innerHTML="This is a test for new div and heading and para";
+newpara.innerHTML="A new para is added to this div element with heading";
+
+document.getElementById("newelement").appendChild(newHeading);
+document.getElementById("newelement").appendChild(newpara);
+
+
+
+var newHeading=document.createElement("h1");
+var newpara=document.createElement("p");
+
+var h1text=document.createTextNode("this is a test for new div element");
+var paratext=document.createTextNode("this is paragraph");
+
+newHeading.appendChild(h1text);
+newpara.appendChild(paratext);
+
+document.getElementById("newelement").appendChild(newHeading);
+document.getElementById("newelement").appendChild(newpara);*/
+
+
+/*var pagecontent=document.getElementById("page_content");
+pagecontent.onclick=function(){
+    alert("you did it");
+}; 
+*/
+
+/*function eventhandlers(){
+    var pagecontent=document.getElementById("page_content");
+    pagecontent.onclick=function(){
+        alert("you did it");
+
+    };
+}
+
+window.onload=function(){
+    //event handlers
+ eventhandlers();
+
+}
+*/
+
+
